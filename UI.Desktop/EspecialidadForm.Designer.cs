f0f711bf0a2a8dc50c5f1a5f80710d77381ce588
atomo <atomotechnology@gmail.com>
2021-07-22 00:43:31 -0300

    My first commit

---
+++ UI.Desktop/EspecialidadForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace UI.Desktop
+{
+    partial class EspecialidadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDescripcion = new System.Windows.Forms.TextBox();
+            this.btnSaveEsp = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbDescripcion
+            // 
+            this.tbDescripcion.Location = new System.Drawing.Point(477, 66);
+            this.tbDescripcion.Multiline = true;
+            this.tbDescripcion.Name = "tbDescripcion";
+            this.tbDescripcion.Size = new System.Drawing.Size(315, 63);
+            this.tbDescripcion.TabIndex = 0;
+            // 
+            // btnSaveEsp
+            // 
+            this.btnSaveEsp.Location = new System.Drawing.Point(368, 205);
+            this.btnSaveEsp.Name = "btnSaveEsp";
+            this.btnSaveEsp.Size = new System.Drawing.Size(205, 58);
+            this.btnSaveEsp.TabIndex = 1;
+            this.btnSaveEsp.Text = "Guardar";
+            this.btnSaveEsp.UseVisualStyleBackColor = true;
+            this.btnSaveEsp.Click += new System.EventHandler(this.btnSaveEsp_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(595, 205);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(197, 58);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Descripcion de la Especialidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(62, 66);
+            this.tbId.Multiline = true;
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(315, 63);
+            this.tbId.TabIndex = 5;
+            // 
+            // EspecialidadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 284);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSaveEsp);
+            this.Controls.Add(this.tbDescripcion);
+            this.Name = "EspecialidadForm";
+            this.Text = "Especialidad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDescripcion;
+        private System.Windows.Forms.Button btnSaveEsp;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbId;
+    }
+}
---
+++ UI.Desktop/UsuarioDesktop.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class UsuarioDesktop 
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UsuarioDesktop));
+            this.lblClave = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtConfirmarClave = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblConfirmarClave = new System.Windows.Forms.Label();
+            this.chkHabilitado = new System.Windows.Forms.CheckBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblClave.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblClave.Location = new System.Drawing.Point(3, 195);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(73, 29);
+            this.lblClave.TabIndex = 3;
+            this.lblClave.Text = "Clave";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblEmail.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblEmail.Location = new System.Drawing.Point(3, 125);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(74, 29);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblNombre.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblNombre.Location = new System.Drawing.Point(3, 59);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(100, 29);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre ";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblID.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblID.Location = new System.Drawing.Point(3, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(36, 29);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.5203F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.80548F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.88574F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.78848F));
+            this.tableLayoutPanel1.Controls.Add(this.txtConfirmarClave, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtClave, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtUsuario, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtEmail, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtApellido, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtNombre, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblID, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblNombre, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblEmail, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblClave, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblApellido, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblUsuario, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblConfirmarClave, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.chkHabilitado, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtID, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(44, 48);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.62801F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.03143F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.23315F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.10741F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(942, 303);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // txtConfirmarClave
+            // 
+            this.txtConfirmarClave.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmarClave.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtConfirmarClave.Location = new System.Drawing.Point(606, 198);
+            this.txtConfirmarClave.Multiline = true;
+            this.txtConfirmarClave.Name = "txtConfirmarClave";
+            this.txtConfirmarClave.PasswordChar = '#';
+            this.txtConfirmarClave.Size = new System.Drawing.Size(292, 51);
+            this.txtConfirmarClave.TabIndex = 17;
+            // 
+            // txtClave
+            // 
+            this.txtClave.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClave.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtClave.Location = new System.Drawing.Point(111, 198);
+            this.txtClave.Multiline = true;
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PasswordChar = '#';
+            this.txtClave.Size = new System.Drawing.Size(312, 51);
+            this.txtClave.TabIndex = 16;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtUsuario.Location = new System.Drawing.Point(606, 128);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(292, 44);
+            this.txtUsuario.TabIndex = 15;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtEmail.Location = new System.Drawing.Point(111, 128);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(312, 44);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtApellido.Location = new System.Drawing.Point(606, 62);
+            this.txtApellido.Multiline = true;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(292, 42);
+            this.txtApellido.TabIndex = 13;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtNombre.Location = new System.Drawing.Point(111, 62);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(312, 42);
+            this.txtNombre.TabIndex = 12;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Bahnschrift", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAceptar.Location = new System.Drawing.Point(473, 369);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(262, 65);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Bahnschrift", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(741, 369);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(245, 65);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblApellido.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblApellido.Location = new System.Drawing.Point(429, 59);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(101, 29);
+            this.lblApellido.TabIndex = 6;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblUsuario.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblUsuario.Location = new System.Drawing.Point(429, 125);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(96, 29);
+            this.lblUsuario.TabIndex = 7;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblConfirmarClave
+            // 
+            this.lblConfirmarClave.AutoSize = true;
+            this.lblConfirmarClave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblConfirmarClave.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmarClave.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblConfirmarClave.Location = new System.Drawing.Point(429, 195);
+            this.lblConfirmarClave.Name = "lblConfirmarClave";
+            this.lblConfirmarClave.Size = new System.Drawing.Size(128, 58);
+            this.lblConfirmarClave.TabIndex = 8;
+            this.lblConfirmarClave.Text = "Confirmar Clave";
+            // 
+            // chkHabilitado
+            // 
+            this.chkHabilitado.AutoSize = true;
+            this.chkHabilitado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkHabilitado.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkHabilitado.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.chkHabilitado.Location = new System.Drawing.Point(429, 3);
+            this.chkHabilitado.Name = "chkHabilitado";
+            this.chkHabilitado.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chkHabilitado.Size = new System.Drawing.Size(142, 33);
+            this.chkHabilitado.TabIndex = 9;
+            this.chkHabilitado.Text = "Habilitado";
+            this.chkHabilitado.UseVisualStyleBackColor = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtID.Location = new System.Drawing.Point(111, 3);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(312, 36);
+            this.txtID.TabIndex = 10;
+            // 
+            // UsuarioDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1040, 456);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnCancelar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UsuarioDesktop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Usuario";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private Label lblClave;
+        private Label lblEmail;
+        private Label lblNombre;
+        private Label lblID;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox txtConfirmarClave;
+        private TextBox txtClave;
+        private TextBox txtUsuario;
+        private TextBox txtEmail;
+        private TextBox txtApellido;
+        private TextBox txtNombre;
+        private Button btnAceptar;
+        private Button btnCancelar;
+        private Label lblApellido;
+        private Label lblUsuario;
+        private Label lblConfirmarClave;
+        private CheckBox chkHabilitado;
+        private TextBox txtID;
+    }
+}
